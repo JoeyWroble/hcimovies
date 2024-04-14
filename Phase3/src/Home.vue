9ca1a9d062710741c0bfffd72b5673634efe04d9
JoeyWroble <jwroble@uiowa.edu>
2024-04-14 17:39:01 -0500

    Card rows!

--- Phase3/src/Home.vue
+++ Phase3/src/Home.vue
@@ -1,33 +1,34 @@
 <template>
-    <div>
-      <div v-for="movie of props.Movies" :key="movie" @click="handleTitleClick(movie)">
- 
-        <!-- <span> -->
-            <p>{{ movie }} </p>
-            <p><img :src="imagesForMovies[props.Movies.indexOf(movie)]" alt="Movie Poster"/></p>
-        <!-- </span> -->
-      </div>
-  
-      <div class="modal fade" id="movieModal" tabindex="-1" aria-labelledby="movieModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="movieModalLabel">Movie Title</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <p><strong>Director:</strong> </p>
-                <p><strong>Genre(s):</strong> </p>
-                <p><strong>Rating:</strong> </p>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            </div>
-          </div>
+  <div class="row row-cols-1 row-cols-md-6 g-4">
+    <div class="col" v-for="movie of props.Movies" :key="movie" @click="handleTitleClick(movie)">
+      <div class="card h-100">
+        <img :src="imagesForMovies[props.Movies.indexOf(movie)]" class="card-img-top" :alt="`${movie} Poster`" style="max-height: 300px; object-fit: cover;">
+        <div class="card-body">
+          <h5 class="card-title">{{ movie }}</h5>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+
+  <div class="modal fade" id="movieModal" tabindex="-1" aria-labelledby="movieModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="movieModalLabel">Movie Title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p><strong>Director:</strong> </p>
+          <p><strong>Genre(s):</strong> </p>
+          <p><strong>Rating:</strong> </p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
   
   <script setup>
   import { defineProps, ref, watch } from 'vue';
